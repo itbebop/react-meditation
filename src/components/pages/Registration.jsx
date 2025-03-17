f183a8f53e274c9ce31b621845d69abaf29f51bb
itbebop <it.bebop@gmail.com>
2025-03-17 22:24:49 +0900

    design: registration - 글자, button 크기 줄임

--- src/components/pages/Registration.jsx
+++ src/components/pages/Registration.jsx
@@ -46,7 +46,7 @@ const Registration = () => {
                 </span>
               </div>
               <button className="text-sm bg-neutralGreen text-white px-4 py-2 rounded-lg hover:bg-lightGreen">
-                신청하기
+                신청
               </button>
             </div>
           </div>
@@ -85,7 +85,7 @@ const Registration = () => {
                 </span>
               </div>
               <button className="text-sm bg-neutralGreen text-white px-4 py-2 rounded-lg hover:bg-lightGreen">
-                신청하기
+                신청
               </button>
             </div>
           </div>

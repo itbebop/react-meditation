ca3439a39284fdebad660bddbded565a358c52e5
itbebop <it.bebop@gmail.com>
2025-03-23 11:31:45 +0900

    fix: vercel 배포 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   resolve: {
     alias: {
-      "@ui": "/src/components/ui", // 예시: @ui를 사용하여 ui 폴더를 참조
+      "@ui": path.resolve(__dirname, "src/components/ui"), // 절대 경로 사용
     },
   },
   plugins: [react()],

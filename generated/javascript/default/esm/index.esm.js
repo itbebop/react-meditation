8590b7796bcc3624025098493d125cb635c174c9
itbebop <it.bebop@gmail.com>
2025-03-27 00:30:20 +0900

    chore: firebase 설정

---
+++ generated/javascript/default/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'react-js-project-202503',
+  location: 'asia-southeast1'
+};
+
---
+++ generated/javascript/default/index.cjs.js
@@ -0,0 +1,9 @@
+const { getDataConnect, validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'react-js-project-202503',
+  location: 'asia-southeast1'
+};
+exports.connectorConfig = connectorConfig;
+
